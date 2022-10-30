535cb67337f53c8e4354836fce389ca566092830
VladKyndiuk <vlad2013649@gmail.com>
2022-10-30 18:55:46 +0200

    upd

--- src/main/webapp/resources/js/script.js
+++ src/main/webapp/resources/js/script.js
@@ -1,30 +1,21 @@
+const transitionEl = document.querySelector(".transition-2");
+
 window.onload = () => {
-    console.log('this is document onload');
-    const transitionEl = document.querySelector(".transition-2");
-    const anchors = document.querySelectorAll('.jump');
+
+    // const transitionEl = document.querySelector(".transition-2");
 
     setTimeout(() => {
         transitionEl.classList.remove("isActive");
     },200)
 
-
-    for(let i = 0; i < anchors.length; i++) {
-        const anchor = anchors[i];
-        anchor.addEventListener("click",e=>{
-            
-            e.preventDefault();
-        
-            let target = e.target.href;
-            transitionEl.classList.add("isActive");
-            setTimeout(() => {
-                window.location.href = target;
-            },200)
-        });
-
-    }
 }
 
-// window.
+
+document.addEventListener('DOMContentLoaded', function(){
+    setTimeout(() => {
+        transitionEl.classList.remove("isActive");
+    },200)
+}
 
 // document.onreadystatechange = () => {
 //     if (document.readyState === 'complete') {
